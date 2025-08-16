fd83db360cdeb12f332c5f24aaa20e8208d817b9
Chandu <chanduthanuku24@gmail.com>
2025-08-16 18:51:20 +0530

    Update max.cpp

--- Leetcode Problems/Leetcode : 53 (Maximum SubArray)/max.cpp
+++ Leetcode Problems/Leetcode : 53 (Maximum SubArray)/max.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum=0,curSum=0;
+        int maxSum=INT_MIN,curSum=0;
         for(int i : nums){
             curSum+=i;
             maxSum=max(maxSum,curSum);
