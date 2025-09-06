10b21b1dffee86d08ad96300625fbfb0ed556d93
Chandu <chanduthanuku24@gmail.com>
2025-09-06 11:02:52 +0530

    Update game.cpp

--- Code cheff/Lead Game/game.cpp
+++ Code cheff/Lead Game/game.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main() {
 int n;
 cin>>n;
-int Si,Ti,lead,maxlead=0,Winner;
+int Si,Ti,sum1=0,sum2=0,lead,maxlead=0,Winner;
 for(int i=0;i<n;i++){
     cin>>Si>>Ti;
-    lead=(Si>Ti)?(Si-Ti):(Ti-Si);
-    int Cwinner=(Si>Ti)?1:2;
+    sum1+=Si;
+   sum2+=Ti;    
+    lead=(sum1>sum2)?(sum1-sum2):(sum2-sum1);
+    int Cwinner=(sum1>sum2)?1:2;
     if(lead>maxlead){
         maxlead=lead;
         Winner=Cwinner;
